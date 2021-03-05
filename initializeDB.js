d9ec6af6378ee6c73e08b66ce1382a60097c8ded
sumedharkulkarni <sumedha.rk.05@gmail.com>
2021-03-05 13:23:08 +0530

    Added postgres integration

--- initializeDB.js
+++ initializeDB.js
@@ -2,10 +2,11 @@ const { Client } = require('pg');
 
 const initDB = () => {
   const client = new Client({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'testdb',
-    password: 'password',
+    user: 'rcizqtkveueezl',
+    host: 'ec2-50-19-176-236.compute-1.amazonaws.com',
+    database: 'ddff0v5frm8fn8',
+    password:
+      '78fb7e4d8d83b9644a86520b832e167ae867ba7cd4394de5af8aa7b6dd644372',
     port: 5432,
   });
 
